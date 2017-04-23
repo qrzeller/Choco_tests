a37ac9d6161573786a5566701bc91a5eb6fc1deb
qrzeller <quentin.zeller@epfl.ch>
2017-04-23 18:04:27 +0200

    Working intVar basic

--- my-choco-Test/src/main/java/com/chocoTest/app/App.java
+++ my-choco-Test/src/main/java/com/chocoTest/app/App.java
@@ -12,11 +12,11 @@ public class App
     {
 
         VariableAndConstraint var = new VariableAndConstraint(24);
-        //var.addTask("Eat",11,14,2);
+        var.addTask("Eat",11,14,2);
         var.addTask("WorkAfternoon", 12,20,5);
         var.addTask("sleepMorning", 1,12,4);
-        //var.addTask("sleepEvening", 19,24,3);
-        //var.addTask("workMorning",4,12,2);
+        var.addTask("sleepEvening", 19,24,3);
+        var.addTask("workMorning",4,12,2);
 
         System.out.println(var.solve());
 
--- my-choco-Test/src/main/java/com/chocoTest/app/model/VariableAndConstraint.java
+++ my-choco-Test/src/main/java/com/chocoTest/app/model/VariableAndConstraint.java
@@ -3,44 +3,57 @@ import org.chocosolver.solver.Solver;
 
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solver;
-import org.chocosolver.solver.variables.SetVar;
+import org.chocosolver.solver.variables.IntVar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by qtask on 23.04.2017.
  */
 public class VariableAndConstraint {
     private Model model;
-    private ArrayList<SetVar> vars;
-    private SetVar universe;
+    private ArrayList<IntVar> vars;
+    private IntVar universe;
     public VariableAndConstraint(int universe){
         model = new Model("A day constraint of " + universe +"slot");
-        vars = new ArrayList<SetVar>(24);
+        vars = new ArrayList<IntVar>(24);
         //this.universe = model.setVar("universe", 1 , 24);
 
     }
 
     public void addTask(String taskName, int from, int to, int duration) {
         for(int i = 0; i < duration ; i++){
-            SetVar var = model.setVar(taskName, from, to);
+            IntVar var = model.intVar(taskName+"_"+i, from, to);
             vars.add(var);
         }
     }
     public String solve(){
         String response = "";
-        SetVar[] array = vars.toArray(new SetVar[0]);
-        model.allDisjoint(array).post();
-        
+        IntVar[] array = vars.toArray(new IntVar[0]);
+        model.allDifferent(array).post();
+
+        String status;
         if(model.getSolver().solve()){
-            response += "Found!";
+            status = "Found!\n";
         }else
         {
-            response += "not found :";
+            status = "not found \n";
         }
-        for(SetVar v :vars){
+        for(IntVar v :vars){
             response += v.getValue() + " : " +v.getName() +"\n";
         }
-        return response;
+        String[] sorted = response.split("\n");
+        Arrays.sort(sorted);
+
+        String result = "";
+        for(String l:sorted){
+            if(!Character.isDigit(l.charAt(1))) result+=l + "\n";
+        }
+        for(String l:sorted){
+            if(Character.isDigit(l.charAt(1))) result+=l + "\n";
+        }
+
+        return status + result;
     }
 }
